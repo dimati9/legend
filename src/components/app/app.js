a5f56e3d545afbada5e7b0da193d6fadf75ff4d0
Dmitriy o_HET <dimati9@yandex.ru>
2021-08-21 00:18:52 +0500

    add server
    add server objects
    set object to map
    add view desc
    add buttons to desc

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,13 +4,21 @@ import './app.css';
 
 class App extends Component {
     state = {
-        objects: [
+        server: "http://localhost:3100",
+        objects: null,
+        objects2: [
             {name:"Fox", coord:[58.52461842, 31.16543115], icon:"fox",desc:"Красивая лисичка здесь пробегала"},
             {name:"Wolf", coord:[58.5661842, 31.36943415], icon:"wolf",desc:"Я злой и страшный серый Волк, я в поросятах знаю толк"},
         ],
+        showDesc: false,
+        descText: "",
     };
 
     componentDidMount() {
+        this.getObjects();
+    }
+
+    setMap = () => {
         function info(text) {
             alert(text);
         }
@@ -21,23 +29,80 @@ class App extends Component {
                 zoom: 12
             });
             this.state.objects.map((object) => {
-                let obj = new window.ymaps.Placemark(object.coord, {}, {
+                let coords = object.coords.split(",");
+                console.log(object.coords)
+                let obj = new window.ymaps.Placemark(coords, {}, {
                     iconLayout: 'default#image',
                     iconImageHref: `./images/map/${object.icon}.png`,
                     iconImageSize: [42, 42],
                     iconImageOffset: [0, 0]
                 });
                 myMap.geoObjects.add(obj)
-                obj.events.add('click', info.bind(this,object.desc));
+                obj.events.add('click', () => {
+                    this.setState(() => {
+                        return {
+                            showDesc: true,
+                            descText: object.text,
+                        }
+                    })
+                });
             });
 
         })
+    }
 
+    closeDesc = () => {
+        this.setState(() => {
+            return {
+                showDesc: false,
+                descText: "",
+            }
+        })
+    }
+
+    getObjects = async () => {
+        const url = `${this.state.server}/getObjects/`;
+        try {
+            const response = await fetch(url, {
+                method: 'GET', // или 'PUT'
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            const json = await response.json();
+            const data = JSON.parse(json.data);
+            if(data !== undefined && data !== null) {
+                this.setState(() => {
+                    return {
+                        objects: data,
+                    }
+                }, () => {
+                    console.log(this.state.objects)
+                    this.setMap();
+                })
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     render() {
+        const {descText,showDesc} = this.state;
         return (
             <div className="map">
+                {showDesc ?
+                <div className="desc">
+                   <div className="text">
+                       {descText}
+                   </div>
+                    <div className="buttons">
+                        <button className="adding">Построить маршрут</button>
+                        <button onClick={this.closeDesc.bind(this)} className="close">Закрыть</button>
+                    </div>
+                </div>
+                    : ""
+                }
                 <div id="map" style={{width: "100vw", height: "100vh"}}></div>
             </div>
         )
